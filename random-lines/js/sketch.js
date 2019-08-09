8298b25c6496c3e8fbe5ef04970fc1a301b0234f
narcomarch <kramoogle@gmail.com>
2019-08-09 10:57:25 +0200

    clean(random-lines): delete title, add remove line "pre-code"

--- random-lines/js/sketch.js
+++ random-lines/js/sketch.js
@@ -2,14 +2,14 @@
 * @Author: OMAO
 * @Date:   2018-09-05 08:12:52
 * @Last Modified by:   OMAO
-* @Last Modified time: 2019-08-05 17:31:09
+* @Last Modified time: 2019-08-09 10:06:54
 */
 
 var delay = 0;
 var lineManager;
 
 function setup() {
-  createCanvas(1920, 1080);
+  //createCanvas(1920, 1080);
   createCanvas(windowWidth, windowHeight);
   frameRate(30);
   lineManager = new LineManager(width, height);
@@ -25,6 +25,11 @@ function mouseClicked(event) {
   event.preventDefault();
   lineManager.addLine();
 }
+
+/*function mouseClicked(event) {
+  event.preventDefault();
+  lineManager.removeLine();
+}*/
 
 function mouseWheel() {
   lineManager.reset();
