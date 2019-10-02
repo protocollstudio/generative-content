1249c0797a43e844001f9d1877fde4fcd6b47169
Antharia <artenischloria@gmail.com>
2019-10-02 10:23:21 +0200

    add import and sketch.js

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ import p5 from 'p5';
 import "p5/lib/addons/p5.sound";
 
 //import * as rl from "./random-lines/js/sketch.js"
-import * as gt from "./glowing-tiles/js/sketch.js"
+// import * as gt from "./glowing-tiles/js/sketch.js"
+import * as ns from "./nico-sketch/js/sketch.js"
--- nico-sketch/js/sketch.js
+++ nico-sketch/js/sketch.js
@@ -1,5 +1,14 @@
 import { parametersPanelManager } from "Modules/ParametersPanelManager.js";
 
 function setup() {
-    
+  createCanvas(windowWidth, windowHeight);
+  background(230);
+  stroke(40);
+  frameRate(30);
+
+  parametersPanelManager.setup(false);
+}
+
+function draw() {
+  ellipse(200, 200);
 }
