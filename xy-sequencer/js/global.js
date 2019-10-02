b4b1a0316e75006af2d1ead0be31dce91487f819
Antharia <artenischloria@gmail.com>
2019-10-02 14:43:29 +0200

    FEAT add global class for inital configuration

---
+++ xy-sequencer/js/global.js
@@ -0,0 +1,30 @@
+class Global {
+  constructor() {
+    this.fgColor;
+    this.bgColor;
+    this.cols;
+    this.rows;
+    this.cellSize;
+    this.squares;
+    this.currentTime;
+    this.speed;
+    this.cursorX;
+    this.cursorY;
+    this.frame;
+  }
+
+  init() {
+    // interface colors
+    this.fgColor = 70;
+    this.bgColor = 240;
+    // sequenceur size
+    this.cols = 2;
+    this.rows = 2;
+    // cursor position & speed
+    this.cursorX = 0;
+    this.cursorY = 0;
+    this.speed = 5;
+    // time management
+    this.frame = 1;
+  }
+}
