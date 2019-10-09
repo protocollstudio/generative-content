57666365dc052d25df0e4730d3a78118c4908fb7
narcomarch <kramoogle@gmail.com>
2019-10-09 12:28:56 +0200

    config: move nico's sketches to sketches folder

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 * @Author: OMAO
 * @Date:   2019-10-09 12:25:06
 * @Last Modified by:   OMAO
-* @Last Modified time: 2019-10-09 12:26:27
+* @Last Modified time: 2019-10-09 12:27:24
 */
 
 import p5 from 'Node/p5';
@@ -12,8 +12,8 @@ import "Node/lodash";
 // sketches list
 import * as randomLines from "Sketches/random-lines/sketch.js"
 import * as glowingTiles from "Sketches/glowing-tiles/sketch.js"
-import * as sphereAgents from "./sphere-agents/js/sketch.js"
-import * as xySequencer from "./xy-sequencer/js/sketch.js"
+import * as sphereAgents from "Sketches/sphere-agents/js/sketch.js"
+import * as xySequencer from "Sketches/xy-sequencer/js/sketch.js"
 
 let currentSketch = xySequencer; // use the sketch you want to see :)
 
